b9cb69d682995b56fcfcc18c547049793e327495
ROHIT ASWAL <97654884+Rohit123Aswal@users.noreply.github.com>
2023-09-20 14:58:42 +0000

    datatypes

---
+++ basics/dataTypes-summary.js
@@ -0,0 +1,37 @@
+// primitive dataTypes
+
+// 7 types : string, number, boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+const isLoggedIn = false
+
+const OutsideTemp = null
+
+let UserEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+
+
+
+// refernce types (Non Primitive dataTypes)
+
+// Arrays, Objects, Functions
+
+const heros = ["ironman", "hulk", "spiderman"];
+let myObj = {
+    name : "rohit",
+    age : 25,
+}
+
+const myfunction =  function(){
+console.log("hello world")
+
+}
+
+console.log(typeof OutsideTemp);
+
