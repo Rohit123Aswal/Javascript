5b464339490d04fdc90fe5bd768ede1275b9f99b
rohitaswal117@gmail.com <rohitaswal117@gmail.com>
2023-12-30 16:07:47 +0530

    2

---
+++ basics/04_control_flow.js/switch.js
@@ -0,0 +1,20 @@
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 3:
+        console.log("april")
+        break;
+
+    default:
+        console.log("default got match")
+        break;
+}
