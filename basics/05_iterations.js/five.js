3bba3a49352ac3322153bebc2ca3c6bfc7fe9873
rohitaswal117@gmail.com <rohitaswal117@gmail.com>
2024-01-03 19:55:32 +0530

    new code

---
+++ basics/05_iterations.js/five.js
@@ -0,0 +1,40 @@
+const coding = ["js", "ruby", "python", "java", "cpp"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+// coding.forEach( (value) => {
+//     console.log(value);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
