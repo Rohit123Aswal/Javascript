eb91b3ff319cc92315716cdb1240a39e633ecb67
rohitaswal117@gmail.com <rohitaswal117@gmail.com>
2024-01-16 20:15:29 +0530

    new

---
+++ basics/06_DOM/script.js
@@ -0,0 +1,99 @@
+let foofItems = ["potato", "apple", "mango", "banana"]
+
+// console.log(foofItems);
+
+// foofItems.push("grapes")
+// console.log(foofItems);
+
+// foofItems.pop();
+// console.log(foofItems);
+// let deletedItem = foofItems.pop()
+// foofItems.pop();
+// console.log(foofItems);
+
+
+
+
+// console.log("deleted", deletedItem);
+
+let heros = ["Bent10", "IronMan", "Superman", "Batman", "Aqauman", "Antman"]
+
+// for (let i = 0; i < heros.length; i++) {
+//     console.log(heros[i]);
+// }
+
+
+// for of
+// for (let hero of heros) {
+//     console.log(hero);
+// }
+
+let cities = ["Mumbai", "Hydrabad", "Delhi", "Kerala"];
+
+// for(city of cities){
+//     console.log(city);
+// }
+
+
+// let marks = [87, 78, 83, 9, 23, 34, 65];
+
+// let sum = 0
+// for (let val of marks) {
+//     // sum = sum + val;
+//     sum += val;
+// }
+// let avg = sum / marks.length;
+// console.log(`avg marks of the class = ${avg}`);
+
+// let items = [878, 788, 83, 879, 23, 348, 65, 100];
+
+// let i = [0];
+
+// for (let val of items) {
+//     console.log(`value at index, ${i} =  ${val}`);
+//     let offer = val / 10;
+//     items[i] = items[i] - offer;
+//     console.log(`value after offer = ${items[i]}`);
+//     i++;
+// }
+
+
+let MarvelHeros = ["Bent10", "IronMan", "Superman", "Batman", "Aqauman", "Antman"]
+// console.log(MarvelHeros);
+
+// console.log(MarvelHeros.slice(2, 3));
+
+
+// splice(add,remove,replace)
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// arr.splice(2,2,101,102);
+// console.log(arr);
+
+
+// Add Element
+// arr.splice(2,0,101)
+
+
+// Delete Element
+// arr.splice(3, 1);
+// console.log(arr)
+
+
+//Replace Element
+// arr.splice(3, 1, 101);
+// console.log(arr);
+
+// let companies = ["Bloomberg", "Microsoft", "uber", "Google", "IBM", "Netflix"]
+// console.log("before", companies);
+// companies.shift()  // shift is used to del the first element from the array
+// console.log("after", companies);
+
+// companies.splice(2,1,"ola")
+// console.log(companies);
+
+// companies.push("Amazon")
+// console.log(companies);
+// companies.unshift("Amazon")  // unshift is used to add the  element in the starting of array
+// console.log(companies);
